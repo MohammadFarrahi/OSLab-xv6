1ad5ee8777313c06e41c491ef848c05bf9f51223
Hesam <hesam.as.sa.as@gmail.com>
2021-11-14 20:46:18 +0330

    Comments Added to calculate_sum_of_digits.c

--- calculate_sum_of_digits.c
+++ calculate_sum_of_digits.c
@@ -11,12 +11,12 @@ int main(int argc, char *argv[])
    }
    int number = atoi(argv[1]);
    int reg_value;
-
-   __asm__("movl %%edx, %0" : "=r" (reg_value));
-   __asm__("movl %0, %%edx" :  : "r"(number) );
+   // We will use edx register for storing input number
+   __asm__("movl %%edx, %0" : "=r" (reg_value)); // reg_value = edx
+   __asm__("movl %0, %%edx" :  : "r"(number) ); // edx = number
 
    calculate_sum_of_digits();
 
-   __asm__("movl %0, %%edx" :  : "r"(reg_value));
+   __asm__("movl %0, %%edx" :  : "r"(reg_value)); //restore edx = reg_value
    exit();
 }
