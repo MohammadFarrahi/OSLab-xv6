aed3f749d755c6a22588829479535efc524cc7d2
Hesam <hesam.as.sa.as@gmail.com>
2021-12-12 14:04:06 +0330

    scheduler changed

--- proc.c
+++ proc.c
@@ -409,31 +409,41 @@ scheduler(void)
 {
   struct proc *p;
   struct cpu *c = mycpu();
-  c->proc = 0;
+  c->proc = NULL;
 
   for(;;){
     // Enable interrupts on this processor.
     sti();
 
-    // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
+    p = get_proc_from_rr_queue();
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+    if(p == NULL)
+      p = get_proc_from_lcfs_queue();
 
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
+    if(p == NULL)
+      p = get_proc_from_mhrrn_queue();
 
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
+    // Switch to chosen process.  It is the process's job
+    // to release ptable.lock and then reacquire it
+    // before jumping back to us.
+    if(p != NULL)
+    {
+        c->proc = p;
+        switchuvm(p);
+        p->state = RUNNING;
+        //p->cycles += 0.1;
+        //update_waiting_times();
+        //p->waiting_time = 0;
+        check_aging();
+        swtch(&(c->scheduler), p->context);
+        switchkvm();
+
+        c->proc = NULL;
+        if (p->state == RUNNABLE && p->queue_num == ROUND_ROBIN)
+        {
+            p->arrival_time = ticks;
+        }
     }
     release(&ptable.lock);
 
