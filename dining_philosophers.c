bea764464f9689fa2fee045938af24d643a78a12
Hesam <hesam.as.sa.as@gmail.com>
2022-01-02 21:03:49 +0330

    added sys_pickup_chopsticks

--- dining_philosophers.c
+++ dining_philosophers.c
@@ -1,1 +1,30 @@
-p
+#include "dining_philosphers.h"
+
+int state[N];
+int phil[N] = { 0, 1, 2, 3, 4 };
+struct semaphore mutex;
+
+
+void sys_pickup_chopsticks(void)
+{
+    int phil_num;
+    if(argint(0, &phil_num) < 0)
+        return -1;
+
+    
+    sem_acquire(&mutex);
+
+        state[phil_num] = HUNGRY;
+
+        printf("Philosopher %d is Hungry\n", phil_num + 1); //***log***
+    
+        // eat if neighbours are not eating
+        test(phil_num);
+
+    sem_release(&mutex);
+ 
+    // if unable to eat wait to be signalled
+    sem_acquire(&S[phil_num]);
+ 
+    sleep(1);
+}
---
+++ dining_philosophers.h
@@ -0,0 +1,11 @@
+#include "semaphore.h"
+
+#define N 5
+#define EATING 0
+#define HUNGRY 1
+#define THINKING 2
+// #define LEFT (phnum + 4) % N
+// #define RIGHT (phnum + 1) % N
+
+
+
