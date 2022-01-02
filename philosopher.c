0f014b9a5a57cdff714424f2f23738140992fb8f
Mohammad Farrahi <farahim.1379@gmail.com>
2022-01-02 20:30:39 +0330

    adding files

---
+++ philosopher.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+#include "fcntl.h"
+
+
+int main(int argc, char *argv[]){
+    
+    int phil_num = atoi(argv[1]);
+    while(1) {
+        sleep(1); // thinking
+        pickup_chopsticks(phil_num);
+        sleep(1); // eating
+        putdown_chopsticks(phil_num);   
+    }
+    exit();
+}
